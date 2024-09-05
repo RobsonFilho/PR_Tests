3bd09b64a25e1958a46d90798c3b951894ad5ae5
Robson Hamilton <robson.filho@nubank.com.br>
2024-09-05 09:29:55 -0300

    test

--- project.clj
+++ project.clj
@@ -1,1 +1,9 @@
-(ns project)
+(defproject pr_tests "1"
+  :description "Just a project to test github actions for a PR"
+  :url "https://github.com/RobsonFilho/PR_Tests"
+  :license {:name "Proprietary"}
+  :dependencies [[org.clojure/clojure "1.11.3"]
+                 [com.datomic/peer "1.0.7187" ]]
+  :repl-options  {:init-ns user}
+  :min-lein-version "2.4.2"
+  :resource-paths ["resources"] )
